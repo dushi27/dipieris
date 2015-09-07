76fe5ef6204ca34de3ed4e54ecf202ac30b58888
dushi27 <dushi27@gmail.com>
2015-09-07 04:11:15 +0000

    add loading animations

---
+++ public/js/bin/custom.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    $('.left').effect('slide', 'slow');
+    $('#safeme').effect('slide', 'slow');
+    $('#githubstats').effect('slide', { direction: "right" }, 'slow');
+    $('#bookz').effect('slide', 'slow');
+});
